a05c93e21fa3d2db9b4bbe7d1339db86b65791d9
tanvx308 <tanvxph13005@gmail.com>
2022-03-25 16:30:31 +0700

    excercise 25/3/2022

---
+++ practice-jpa-2/src/main/java/fis/java/topic13/PracticeJpa2Application.java
@@ -0,0 +1,13 @@
+package fis.java.topic13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeJpa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeJpa2Application.class, args);
+	}
+
+}
